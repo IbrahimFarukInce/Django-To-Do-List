db3c41f7f0c420300d1caaea09743df567219da9
ibrahim ince <ibrahimfarukince@gmail.com>
2023-11-11 05:02:37 +0300

    user registeration

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,12 +1,16 @@
 from django.forms.models import BaseModelForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import FormView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth import login
+
 
 
 from . models import Task
@@ -18,6 +22,28 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+    
+
+class UserRegister(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm  
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    success_url = reverse_lazy('tasks')
+
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
+    def get(self, *args, **kwagrs):
+            if self.request.user.is_authenticated:
+                return redirect('tasks')
+            return super().get( *args, **kwagrs)
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
